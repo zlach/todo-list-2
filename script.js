f1d29f3248ba13c0c50a3384588d1f561fef93ff
Zachary Goldberg <zacharysp@gmail.com>
2019-03-25 20:21:04 -0500

    time to style

---
+++ script.js
@@ -0,0 +1,86 @@
+
+
+// var button = document.getElementById("#submit-btn"), count = 0;
+
+// button.onclick = function(){
+//     count += 1;
+// };
+
+// var n = 0;
+
+// function increment(){
+//     n++;
+//     return n;
+// }
+
+let todo_array = [];
+
+function doEntry(){
+
+    console.clear();
+
+    let todo_item = document.querySelector("#task-input");
+
+    if (todo_item.value === "") {
+        alert("Need A Value")
+    } else {
+        todo_array.push(todo_item.value);
+
+        console.table(todo_array)
+
+        console.log(todo_array[0])
+
+        console.log(todo_array)
+
+        listEntries();
+
+        todo_item.value = "";
+    }
+
+    return false;
+}
+
+function listEntries(){
+    var list = document.getElementById("entries")
+
+    list.innerHTML = '';
+
+    if (todo_array.length !== 0) {
+        for(var i = 0; i < todo_array.length; i++) {
+            var iDiv = document.createElement('div');
+            iDiv.className = 'entry';
+            iDiv.innerHTML = todo_array[i] + `<button class='x' onclick='remove(${i})'>X</button>`;
+            document.querySelector("#entries").appendChild(iDiv);
+        }
+    }
+}
+
+function remove(item){
+
+    todo_array.splice(item, 1);
+
+    listEntries();
+}
+
+// function returnEntry(todo_item){
+//     var iDiv = document.createElement('div');
+//     iDiv.className = 'entry';
+//     iDiv.innerHTML = todo_item.value + "<button class='x' onclick='remove()'>X</button>";
+//     document.querySelector("#entries").appendChild(iDiv);
+// }
+
+// function returnEntry(todo_array){
+//     let entries = document.querySelector('#entries');
+//     entries.innerHTML = todo_array.value;
+// }
+
+// var button = document.getElementById("#submit-btn");
+
+// button.addEventListener("click", counter());
+
+// function counter(){
+//     let x = 0;
+//     x += 1;
+// }
+
+// remove
